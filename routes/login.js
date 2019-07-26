a76f14f6fcc8c5d52cc5a103ea45bc697c492f7b
lushilling <lshilling@hotmail.co.uk>
2019-07-26 09:39:58 +0100

    create get and post methods

--- routes/login.js
+++ routes/login.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const router = express.Router();
 
-//Add a user
+let array = []; //Delete later on
 
-
-// @route   GET item/test
+// @route   GET login/test
 // @desc    Tests route
 // @access  Public
 router.get("/test", (req, res) => {
     res.json({
-      message: "Login route works"
+        message: "Login route works"
     });
-  });
+});
+
+//Add user
+// @route   POST login/create
+// @desc    Create a user
+// @access  Public
+router.post("/create", (res, req) => {
+
+    const newLogin = {
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    array.push(newLogin);
+
+    res.send("success");
+
+});
+
+
+// @route   GET login/all
+// @desc    Get all items
+// @access  Public
+
+router.get("/all", (req, res) => {
+    res.send(array);
+});
+
 
 module.exports = router;
