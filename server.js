98f4a5f4b9c5e87664f9b4f8b8caf7b9a13fcde0
lushilling <lshilling@hotmail.co.uk>
2019-07-26 09:12:09 +0100

    Node Assessment

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+//User login
+const login = require("./routes/login.js");
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use("/login", login);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server running on port ${port}`));
