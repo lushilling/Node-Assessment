0f86e441732415adfa7cc8a9011993fd9fdfbd39
lushilling <lshilling@hotmail.co.uk>
2019-07-26 11:46:00 +0100

    coding login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const Login = require("../models/schema");
-const bcrypt = require("bcryptjs");
 const loginValidation = require("../validation/login");
-// const _=require("lodash");
+
+
+// const bcrypt = require("bcryptjs");
+// // const _=require("lodash");
 
 
 // @route   GET login/test
@@ -15,57 +17,72 @@ router.get("/test", (req, res) => {
     });
 });
 
-//@route   GET login/all
-//@desc    Get all usernames
-//@access  Public
-router.get("/all", (req, res) => {
-    const errors = {};
-    Login.find({}, '-password')
-        .then(logins => {
-            if (!logins) {
-                errors.onLogins = "There are no users";
-                res.status((404).json(errors))
-            }
-            res.json(logins);
-        })
-        .catch(err => res.status(404).json({ Message: "There are no users" }));
-});
-
-// @route   POST login/addUser
-// @desc    Add user
+// @route   GET login/userDetails
+// @desc    Get all items from user email and password
 // @access  Public
-router.post("/create", (req, res) =>{
+router.get("/userDetails", (req, res) => {
     const {errors, isValid} = loginValidation(req.body);
     if (!isValid) {
         return res.status(400).json(errors);
     };
-    const login = new Login({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    });
-    bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(login.password, salt, (err, hash) =>{
-            if (err) throw err;
-            login.password = hash;
-            login.save()
-            .then(() => {
-                res.json(login)
-            })
-            .catch(err => res.status(404).json(err));
-        });
-    });
-});
-
-// @route   DELETE login/deleteUsername
-// @desc    Delete items from one username
-// @access  Public
-router.delete("/delete", (req, res) => {
-    Login.deleteOne({'email': req.body.email})
-    .then(({ok, n}) => {
-        res.json({ Login: "Deleted :)" });
-    })
-      .catch(err => res.status(404).json({ Message: "Can not delete login" }));
+    Login.find({'email': req.body.email}, {'password': req.body.password}, '-password')
+      .then(logins => {
+        if (!logins) {
+          errors.noLogins = "Incorrect details";
+          res.status(404).json(errors);
+        }
+        res.json("Login Successful");
+      })
+      .catch(err => res.status(404).json({ Message: "User does not exist" }));
   });
+  
+
+// get user details from email and password and return success
+
+
+
+
+
+
+
+
+
+
+// // @route   POST login/addUser
+// // @desc    Add user
+// // @access  Public
+// router.post("/create", (req, res) =>{
+//     const {errors, isValid} = loginValidation(req.body);
+//     if (!isValid) {
+//         return res.status(400).json(errors);
+//     };
+//     const login = new Login({
+//         username: req.body.username,
+//         email: req.body.email,
+//         password: req.body.password
+//     });
+//     bcrypt.genSalt(10, (err, salt) => {
+//         bcrypt.hash(login.password, salt, (err, hash) =>{
+//             if (err) throw err;
+//             login.password = hash;
+//             login.save()
+//             .then(() => {
+//                 res.json(login)
+//             })
+//             .catch(err => res.status(404).json(err));
+//         });
+//     });
+// });
+
+// // @route   DELETE login/deleteUsername
+// // @desc    Delete items from one username
+// // @access  Public
+// router.delete("/delete", (req, res) => {
+//     Login.deleteOne({'email': req.body.email})
+//     .then(({ok, n}) => {
+//         res.json({ Login: "Deleted :)" });
+//     })
+//       .catch(err => res.status(404).json({ Message: "Can not delete login" }));
+//   });
 
 module.exports = router;
--- validation/login.js
+++ validation/login.js
@@ -5,22 +5,8 @@ module.exports = function validateLoginInput(login) {
     let errors = {};
 
     // if these values are not present in the user object we are validating then they will be set to empty strings for the Validator.isEmpty
-    login.username = !isEmpty(login.username) ? login.username : "";
     login.email = !isEmpty(login.email) ? login.email : "";
     login.password = !isEmpty(login.password) ? login.password : "";
-
-    //username validation rules
-    if (Validator.isEmpty(login.username)) {
-        errors.username = "Username field is required";
-    }
-
-    if (!Validator.isLength(login.username, { min: 3, max: 20 })) {
-        errors.username = "Username is invalid";
-    }
-
-    if (!Validator.isAlphanumeric(login.username)) {
-        errors.username = "Username must contain only letters and numbers";
-    }
 
     //email validation rules
     if (Validator.isEmpty(login.email)) {
